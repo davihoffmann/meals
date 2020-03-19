5e7baf4594b37e671cee66e934fa8c0ff6bad469
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-03-19 10:29:13 -0300

    criado detalhe de meals e estilizado a mesma

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screens/meal_detail_screen.dart';
 import 'screens/categories_screen.dart';
 import 'screens/categories_meals_screen.dart';
 import 'utils/app-routes.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.purpleAccent,
-        fontFamily: 'Raleway',
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        textTheme: ThemeData.light().textTheme.copyWith(
-          title: TextStyle(
-            fontSize: 20,
-            fontFamily: 'RobotoCondensed',
-          )
-        )
-      ),
+          primarySwatch: Colors.purple,
+          accentColor: Colors.purpleAccent,
+          fontFamily: 'Raleway',
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          textTheme: ThemeData.light().textTheme.copyWith(
+                  title: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ))),
       //home: CategoriesScreen(),
       routes: {
-        AppRoutes.HOME : (ctx) => CategoriesScreen(),
-        AppRoutes.CATEGORIES_MEALS : (ctx) => CategoriesMealsScreen(),
+        AppRoutes.HOME: (ctx) => CategoriesScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == '/alguma-coisa') {
+          return null;
+        } else {
+          return MaterialPageRoute(
+            builder: (_) {
+              return CategoriesScreen();
+            },
+          );
+        }
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (_) {
+            return CategoriesScreen();
+          },
+        );
       },
     );
   }
